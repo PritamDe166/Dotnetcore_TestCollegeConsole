12db0bee60f7a862fa502ed8487555146e11b124
PritamDe166 <Pritamde166@gmail.com>
2025-03-14 22:02:21 +0530

    Initial commit

---
+++ GlobalUsings.cs
@@ -0,0 +1,4 @@
+﻿global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using CollegeClassLib;
+global using CollegeClassLib.Service.Interfaces;
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+#region ConfigureServices
+
+var host = Host.CreateDefaultBuilder(args)
+            .ConfigureServices((context, services) =>
+            {
+                services.AddStudentServices();
+            })
+            .Build();
+#endregion
+
+#region StudentService
+
+var studentService = host.Services.GetRequiredService<IStudentService>();
+Console.WriteLine(studentService.GetStudentsList());
+
+#endregion
+
+Console.ReadLine();
+
